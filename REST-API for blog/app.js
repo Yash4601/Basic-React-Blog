34c9a6f3b31cd293b43b871a582c757745c07c5b
Yash4601 <53232830+Yash4601@users.noreply.github.com>
2021-06-04 01:42:07 +0530

    Uploading decoupled frontend and backened

---
+++ REST-API for blog/app.js
@@ -0,0 +1,79 @@
+const path = require('path');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const {
+  v4: uuidv4
+} = require('uuid');
+
+const feedRoutes = require('./routes/feed')
+const authRoutes = require('./routes/auth')
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, uuidv4() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(express.json());
+app.use(
+  multer({
+    storage: fileStorage,
+    fileFilter: fileFilter
+  }).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')))
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({
+    message: message,
+    data: data
+  })
+})
+
+mongoose.connect(
+    'mongodb+srv://YashAgrawal:28uZXb8zHaElxUQa@cluster0.gxlzm.mongodb.net/messages?', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    }).then(result => {
+    const server = app.listen(8000);
+    const io = require('./socket').init(server);
+    io.on('connection', socket => {
+      console.log('Client connected');
+    });
+  })
+  .catch(err => console.log(err))
